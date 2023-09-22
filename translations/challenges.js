911341de505eff059dbe2a3d5b130e10f4699364
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2023-09-22 16:45:05 +0200

    Feat: Framework for challenge translation

---
+++ translations/challenges.js
@@ -0,0 +1,12 @@
+module.exports = challanges_transelation = {
+    "en": {
+        "1": {
+            "titel": "Start",
+        }
+    },
+    "de": {
+        "1": {
+            "titel": "Start",
+        }
+    },
+}
